2ba9d0820cc8086afd760b7749525e78434b4680
성시우 <siwoo01231324@gmail.com>
2025-11-17 14:48:10 +0900

    feat : 실습 상 모든 파일 삭제

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/")
-def home(req:Request):
-    return templates.TemplateResponse("index.html",{"request":req}) 
